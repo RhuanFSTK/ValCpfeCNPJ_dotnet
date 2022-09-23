66a08468616048dc9d6c373ba9162b6c1b4c6c3b
rhuan <rhuan@RHUAN>
2022-09-23 14:39:51 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValCpfeCNPJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CPFCNPJ.IMain checkCFPCNPJ = new CPFCNPJ.Main();
+            var resultCNPJ = checkCFPCNPJ.IsValidCPFCNPJ("CNPJ");
+
+            Console.WriteLine(resultCNPJ);
+
+            var resultCPF = checkCFPCNPJ.IsValidCPFCNPJ("CPF");
+
+            Console.WriteLine(resultCNPJ);
+        }
+    }
+}
